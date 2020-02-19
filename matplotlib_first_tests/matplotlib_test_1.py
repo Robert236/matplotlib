8ed6f2083db6125775cd90b6b0189bd727ae0269
robert <robertvater1@googlemail.com>
2020-02-19 20:04:57 +0100

    first push

---
+++ matplotlib_first_tests/matplotlib_test_1.py
@@ -0,0 +1,15 @@
+from matplotlib import pyplot as plt
+x = [1,2,3]
+y = [5,7,4]
+
+x2 = [1,2,3]
+y2= [10, 14, 12]
+
+plt.plot(x, y, label='First Line')
+plt.plot(x2, y2, label='Sacond Line')
+plt.xlabel('Plot Number')
+plt.ylabel('Important var')
+plt.title('interessting Graph\nCheck that out')
+plt.legend()
+plt.show()
+
