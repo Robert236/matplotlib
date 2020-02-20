c64bc1f9f3644b9e4ae3dba9d6804835ce20e5e5
robert <robertvater1@googlemail.com>
2020-02-20 21:33:16 +0100

    historgram

--- matplotlib_first_tests/matplotlib_test_3.py
+++ matplotlib_first_tests/matplotlib_test_3.py
@@ -4,7 +4,7 @@ population_ages = [22,55,62,45,21,22,34,42,42,4,99,102,110,120,121,122,130,111,1
 # ids = [x for x in range(len(population_ages))]
 bins = [0,10,20,30,40,50,60,70,80,90,100,110,120]
 
-plt.hist(population_ages, bins, histtype='bar', rwidth=0.8)
+plt.hist(population_ages, bins, histtype='bar', rwidth=0.5)
 
 plt.xlabel('x')
 plt.ylabel('y')
