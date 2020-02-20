bfc0b877fe7ab999a3f2dd95a7fb95efa1734270
robert <robertvater1@googlemail.com>
2020-02-20 21:48:39 +0100

    scatterplot

--- matplotlib_first_tests/matplotlib_test_4.py
+++ matplotlib_first_tests/matplotlib_test_4.py
@@ -2,10 +2,11 @@ from matplotlib import pyplot as plt
 
 x = [1,2,3,4,5,6,7,8]
 y = [5,2,4,2,1,4,5,2]
+
 plt.scatter(x,y, label='skitscat', color='k', marker='*', s=150)
 
 plt.xlabel('x')
 plt.ylabel('y')
 plt.title('interessting Graph\nCheck that out')
-#plt.legend()
+plt.legend()
 plt.show()
