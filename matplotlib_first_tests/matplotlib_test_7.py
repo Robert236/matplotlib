d526641d29dec2326ec91a0597d113090053180a
robert <robertvater1@googlemail.com>
2020-02-24 19:10:15 +0100

    load data from file

---
+++ matplotlib_first_tests/matplotlib_test_7.py
@@ -0,0 +1,28 @@
+from matplotlib import pyplot as plt
+import numpy as np
+
+"""
+# Part 1
+import csv
+
+x = []
+y = []
+
+with open('example.txt', 'r') as csvfile:
+    plots = csv.reader(csvfile, delimiter=',')
+    for row in plots:
+        x.append(int(row[0]))
+        y.append(int(row[1]))
+
+plt.plot(x, y, label='Loaded from file')
+"""
+
+x, y = np.loadtxt('example.txt', delimiter=',', unpack=True)
+plt.plot(x, y, label='Loaded from file')
+
+
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('interessting Graph\nCheck that out')
+plt.legend()
+plt.show()
